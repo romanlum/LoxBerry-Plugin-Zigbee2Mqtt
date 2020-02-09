c15a7326f9d7d85935f8dfd99a1689bebe0654ee
Roman Lumetsberger <roman.lumetsberger@outlook.com>
2020-02-09 21:20:47 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LoxBerry-Plugin-Zigbee2Mqtt
+
+This plugin packages Zigbee2Mqtt for loxberry (https://www.zigbee2mqtt.io/)
---
+++ sbin/somesystemscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Can be executed with sudo as user "loxberry".
+echo "A script which can be executed with sudo as user loxberry."
