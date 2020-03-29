a61691dcebd42992cbd2831cce8369cf750f51b5
loxberry <loxberry@loxberry1>
2020-03-29 10:36:23 +0200

    move 3rd party js scripts to vendor folder

--- webfrontend/htmlauth/include/plugin.php
+++ webfrontend/htmlauth/include/plugin.php
@@ -34,7 +34,7 @@ class Plugin
         $navbar[2]['Name'] = $L["Navbar.Devices"];
         $navbar[2]['URL'] = 'devices.php';
         $navbar[2]['active'] = null;
-        $navbar[2]['Script'] = array('ace.js', 'devices.js');
+        $navbar[2]['Script'] = array('vendor/ace.js', 'devices.js');
 
         $navbar[99]['Name'] = $L["Navbar.Logfiles"];
         $navbar[99]['URL'] = 'log.php';
