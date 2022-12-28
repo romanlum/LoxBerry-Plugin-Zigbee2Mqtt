432d5894922bb4b9e94efdc9489768a00b7ae856
Roman Lumetsberger <romanlum@users.noreply.github.com>
2022-12-28 15:41:51 +0000

    fix internal mqtt in lb>=3

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -9,6 +9,10 @@ $twig = Plugin::initializeTwig();
 Plugin::createHeader(1);
 
 $mqtt_installed = LBSystem::plugindata('mqttgateway') ? true : false;
+//mqtt is not a plugin anymore in lb >=3
+if(str_starts_with(LBSystem::lbversion,"3")){
+    $mqtt_installed = true;
+}
 echo $twig->render('index.html', array("mqtt_installed" => $mqtt_installed));
 
 //creates the footer
